2fc489c4335e6fc67090d6bf74c874c0f80faf90
SHIN SUJUNG <shinssjj79@gmail.com>
2024-05-02 20:24:29 +0900

    Initial commit

---
+++ src/test/java/com/nado/NadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
